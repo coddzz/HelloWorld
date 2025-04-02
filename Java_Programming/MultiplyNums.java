6c7c0ef23e1d448348422e6b7a926e61733c040b
Sarath <learn.cypher@gmail.com>
2025-04-02 23:23:31 +0530

    Multiply Two Numbers

---
+++ Java_Programming/MultiplyNums.java
@@ -0,0 +1,10 @@
+public class MultiplyNums {
+    public static void main(String[] args){
+
+        double first = 2.5, second = 5.5;
+        double product = first * second;
+        System.out.println("The product is : "+ product);
+
+    }
+}
+
