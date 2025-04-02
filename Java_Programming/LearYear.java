16924f01c90c393f41ab65d21db485c4fd02ebae
Sarath <learn.cypher@gmail.com>
2025-04-03 00:01:27 +0530

    Leap Year

---
+++ Java_Programming/LearYear.java
@@ -0,0 +1,14 @@
+public class LearYear {
+    
+    public static void main(String[] args){
+
+        int year = 2024;
+        if (year % 4 == 0){
+            System.out.println("leap year (" + year + ")");
+        } else {
+            System.out.println("Not leap year");
+        }
+        
+    }
+    
+}
